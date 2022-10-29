329301c48e6d1bc54f61d42bc3d7c052acc9a25a
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2022-10-29 21:44:55 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
-var screen=document.getElementById('screen');
-function btnClick(value){
-    screen.value+=value;
+var screen=document.getElementById('screenvalue');
+
+function myMessage() {
+  }
+function btnClick(val){
+    
+    screen.value+=val;
 }
 
 function result(){
     screen.value=eval(screen.value);
 }
 
-function clearScreen(){
-    screen.value = "";
+function clean(){
+    console.log("hsam");
+    screen.value="";
 }
