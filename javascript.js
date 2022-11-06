5b1a27a8f9e738de9d3a7a8808975fc9c02286e4
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2022-11-06 18:25:56 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
 var screen=document.getElementById('screenvalue');
+let bgColor=document.getElementById('whole');
 // screenvalue is id of the calculator(text box input) screen
-
 function btnClick(val){//only for when numbers clicked
     screen.value+=val;
     //appends the new clicked value
 }
 
 function result(){//when '=' clicked
-    screen.value=eval(screen.value);
+
+    try{
+        bgColor.style.setProperty("--r",0);
+        bgColor.style.setProperty("--g",137);
+        bgColor.style.setProperty("--b",0);
+
+        screen.value=eval(screen.value);
+    }
+    catch(e){
+        bgColor.style.setProperty("--r",137);
+        bgColor.style.setProperty("--g",0);
+        bgColor.style.setProperty("--b",0);
+        screen.value="";
+    }
 }
 
 function clean(){//when 'c' clicked
